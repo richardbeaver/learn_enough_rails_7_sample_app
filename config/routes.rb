3919d4866db3e51de4ade96554994a63d75caae5
Richard Beaver <beaver.r@northeastern.edu>
2023-08-10 15:19:28 -0400

    Create test for getting about page, and implement

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
 
   # Defines the root path route ("/")
   root "application#hello"
